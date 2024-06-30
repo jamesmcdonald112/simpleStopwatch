5ce6afa104f1fffdea82c0b20befa2773a590472
James McDonald <jamesmcdonald112@yahoo.ie>
2024-06-30 20:23:30 +0100

    Notes of what to do for the start timer

---
+++ script.js
@@ -0,0 +1,30 @@
+let startTime;
+let elapsedTime = 0;
+let intervalId;
+let running = false;
+
+// HTML Elements
+const timer = document.getElementById("timer");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const lap = document.getElementById("lap");
+
+
+function startTimer() {
+    // if not running
+    // Set find start time
+    // IntervalidId - setInterval for upadting the display
+    // Set running to true
+    
+}
+
+function updateDisplay() {
+    // Set elapsed time to current time - start time
+    // Convert to hours minutes and seconds and milliseconds if needed
+    // Format the time to a stign and use the .padStart method
+    // set the contet for the id of timer to have the formated time
+    
+}
+
+// Strat
